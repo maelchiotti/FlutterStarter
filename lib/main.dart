76d9732c9df41f9b757286ff7dce28d263e999f2
Maël Chiotti <mael.chiotti@qiminfo.com>
2023-09-07 16:38:09 +0200

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:starter/app.dart';
 import 'package:starter/utils/package_info_manager.dart';
 import 'package:starter/utils/preferences/preferences_manager.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await PreferencesManager().init();
   await PackageInfoManager().init();
+
+  FlutterNativeSplash.remove();
 
   runApp(App());
 }
