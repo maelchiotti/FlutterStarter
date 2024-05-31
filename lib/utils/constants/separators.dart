d5e63cf58aa9a53e40abfd7acdf000e27f8675c1
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2024-05-31 17:12:39 +0200

    fix: update separators

--- lib/utils/constants/separators.dart
+++ lib/utils/constants/separators.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
 
 enum Separator {
-  divider1(1, 1, 0, 0),
+  divider1indent8(1, 1, 8, 8),
   divider1indent16(1, 1, 16, 16),
-  divider2(2, 2, 0, 0),
-  divider2indent16(2, 2, 16, 16),
   ;
 
-  final double _height;
+  final double _size;
   final double _thickness;
   final double _indent;
   final double _endIndent;
 
-  Divider get divider => Divider(
-        height: _height,
-        thickness: _thickness,
-        indent: _indent,
-        endIndent: _endIndent,
-      );
+  Divider get horizontal => Divider(
+    height: _size,
+    thickness: _thickness,
+    indent: _indent,
+    endIndent: _endIndent,
+  );
+
+  VerticalDivider get vertical => VerticalDivider(
+    width: _size,
+    thickness: _thickness,
+    indent: _indent,
+    endIndent: _endIndent,
+  );
 
   const Separator(
-    this._height,
-    this._thickness,
-    this._indent,
-    this._endIndent,
-  );
+      this._size,
+      this._thickness,
+      this._indent,
+      this._endIndent,
+      );
 }
