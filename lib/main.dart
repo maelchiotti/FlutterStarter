5e35d84423d09ba84c47f00b6b83fb7f9c9b5bd6
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2024-05-31 17:21:49 +0200

    fix: init utils

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:starter/app.dart';
+import 'package:starter/utils/info_utils.dart';
 import 'package:starter/utils/preferences/preferences_utils.dart';
+import 'package:starter/utils/theme_utils.dart';
 
 Future<void> main() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -18,6 +20,8 @@ Future<void> main() async {
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await PreferencesUtils().init();
+  await InfoUtils().init();
+  await ThemeUtils().init();
 
   FlutterNativeSplash.remove();
 
